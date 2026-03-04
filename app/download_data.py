7713abc8bea5cf70d615a8e2da023a0b8b4fae7e
Estêvão Valente Fernandes <70576@novasbe.pt>
2026-03-04 10:41:47 +0000

    changing share_degraded_land url

--- app/download_data.py
+++ app/download_data.py
@@ -87,7 +87,7 @@ def download_required_datasets(
         ),
         DatasetSpec(
             filename="share_degraded_land.csv",
-            url="https://ourworldindata.org/grapher/share-degraded-land.csv",
+            url="https://ourworldindata.org/grapher/share-degraded-land.csv?v=1&csvType=full&useColumnShortNames=true",
         ),
         DatasetSpec(
             filename="red_list_index.csv",
